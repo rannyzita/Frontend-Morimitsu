a961b0917018ceac95d1b8b9ad834ea4730dfc46
rannyzita <fs.vitoria.soares@gmail.com>
2025-11-04 20:05:08 -0300

    Mudando a quantidade de estudante por página na tela de celular;
    e deixando fixo a parte de quando so tem 1 aluno, pra tela nao se achatar (isso no ipad)

--- src/pages/UserManagement/options/ReadUser.tsx
+++ src/pages/UserManagement/options/ReadUser.tsx
@@ -95,7 +95,7 @@ export const VerUsuarios: FC = () => {
 
     useEffect(() => {
         const updateStudentsPerPage = () => {
-            const newStudentsPerPage = window.innerWidth < 768 ? 4 : 5; // 4 mobile, 5 iPad/Desktop
+            const newStudentsPerPage = window.innerWidth < 768 ? 5 : 5; // 4 mobile, 5 iPad/Desktop
             setStudentsPerPage(newStudentsPerPage);
             if (newStudentsPerPage !== studentsPerPage) setCurrentPage(1);
         };
@@ -143,7 +143,7 @@ export const VerUsuarios: FC = () => {
 
                     {/* Lista de alunos */}
                     <div
-                        className={`flex-1 ${mobileHeightClass} flex flex-col gap-6 items-center overflow-y-auto pr-0 md:pr-2 mt-2 md:mt-4 mb-20`}
+                        className={`flex-1 ${mobileHeightClass} flex flex-col gap-6 items-center overflow-y-auto pr-0 md:pr-2 mt-2 md:mt-4 mb-20 md:min-h-[450px] lg:min-h-[500px]`}
                     >
                         {currentAlunos.map((aluno) => (
                             <StudentListItem
