4362d9521517bc798617014250cc00b06e6a3282
rannyzita <fs.vitoria.soares@gmail.com>
2025-11-11 10:58:24 -0300

    Ajustando a rota da tela de relatorio, graduação e aniversariantes;
    fazendo a estrutura das telas de graduação, aniversario e de relatorio;

--- src/pages/birthday/birthday.tsx
+++ src/pages/birthday/birthday.tsx
@@ -1,14 +1,14 @@
 import { useState, type FC, type ReactNode } from 'react';
 import { Box } from '@mui/material';
 import { PageLayout } from '../../components/layout/BigCardGray_';
-import { NotepadText } from 'lucide-react';
+import { Cake } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 
-export const Report: FC = () => {
+export const Birthday: FC = () => {
     return (
         <Box component='div' className='flex flex-col items-center justify-center h-full p-4'>
-            <PageLayout backPath='/home' icon={<NotepadText size={36} className='lg:w-[50px] lg:h-[50px]' />} title='RELATÓRIO'>
+            <PageLayout backPath='/home' icon={<Cake size={36} className='lg:w-[50px] lg:h-[50px]' />} title='ANIVERSARIANTES'>
                 <div className='flex flex-col pb-18 lg:pb-0 gap-6 mt-10 lg:mt-14 items-center'>
 
                 </div> 
