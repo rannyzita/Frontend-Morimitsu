11fcc20ee91e325fa662ac65d749a1bafd8cd03b
rannyzita <fs.vitoria.soares@gmail.com>
2025-11-19 16:28:01 -0300

    Ajustando a parte de Ranking; Fazendo o diagrama de ranking; tornando o RankingCard um componente reutilizável;

--- src/pages/report/report.tsx
+++ src/pages/report/report.tsx
@@ -39,27 +39,76 @@ const RankingCard: FC = () => (
         <Typography variant='h4' className='!font-bold !text-[30px] tracking-wide text-center'>
             MAIOR PRESENÇA
         </Typography>
+
         <Divider className='!border-[#3E0404] !border-[2px] !my-3' />
         
         <Box className='flex flex-col lg:flex-row gap-6 mt-4 h-113'>
-            <Box className='lg:w-1/2 flex flex-col items-center gap-4 bg-[#500000] border-3 border-[#3E0404] !rounded-[5px]'>
+            <Box className='lg:w-1/2 flex flex-col items-center gap-4 bg-[#500000] border-3 border-[#3E0404] !rounded-[5px] shadow-[0_5px_15px_rgba(0,0,0,0.4)]'>
                 <Typography variant='h5' className='!font-semibold text-white pt-4'>RANKING</Typography>
-                <Trophy size={48} className='text-white' />
-                <Typography className='text-xl'>JOÃO LUCAS</Typography>
+                <Trophy size={62} className='text-white' strokeWidth={1.5} />
                 
-                <Box className='flex flex-col gap-2 w-full mt-4 border-2 border-[#3E0404]'>
-                    <Typography className='!font-bold text-center pt-4'>ALUNOS</Typography>
-                    <div className='bg-red-900 p-2 rounded text-sm flex justify-between'>
-                        <span>ANA LAURA</span><span>10x</span>
+                {/* PÓDIO */}
+                <div className='w-full flex justify-center mt-6'>
+                <div className='flex items-end text-white font-bold select-none'>
+
+                    {/* 2º Lugar */}
+                    <div className='bg-[#500000] w-24 h-20 flex flex-col items-center justify-center relative border-t-4 border-[#3E0404]'>
+                    <span className='absolute top-1 text-[10px] tracking-wide'>JOÃO LUCAS</span>
+                    <span className='text-xl mt-4'>2º</span>
                     </div>
-                    <div className='bg-red-900 p-2 rounded text-sm flex justify-between'>
-                        <span>JOÃO LUCAS</span><span>9x</span>
+
+                    {/* 1º Lugar */}
+                    <div className='bg-[#500000] w-28 h-28 mx-2 flex flex-col items-center justify-center relative border-t-4 border-[#3E0404]'>
+                    <span className='absolute top-1 text-[12px] tracking-wide'>ANA LAURA</span>
+                    <span className='text-3xl mt-3'>1º</span>
+                    </div>
+
+                    {/* 3º Lugar */}
+                    <div className='bg-[#500000] w-24 h-16 flex flex-col items-center justify-center relative border-t-4 border-[#3E0404]'>
+                    <span className='absolute top-1 text-[10px] tracking-wide'>NICHOLAS ALVES</span>
+                    <span className='text-xl mt-4'>3º</span>
+                    </div>
+
+                </div>
+                </div>
+                
+                <Box className='flex flex-col gap-3 w-full mt-auto mb-4 px-3'>
+                
+                    <Divider className='!border-[#3E0404] !border-[2px]' />
+
+                    <Typography className='!font-bold text-center'>ALUNOS</Typography>
+
+                    {/* ITEM 1 */}
+                    <div className='bg-[#690808] p-1 rounded-xl flex justify-between items-center  shadow-[0_5px_15px_rgba(0,0,0,0.4)]'>
+                        <div className='flex items-center gap-1'>
+                            <img src='/avatar1.png' className='w-4 h-4 rounded-full' />
+                            <span className='font-bold text-[10px]'>ANA LAURA</span>
+                        </div>
+                        <button className='bg-[#3E0404] px-3 py-1 rounded-md text-[10px]'>Ver mais</button>
+                    </div>
+
+                    {/* ITEM 2 */}
+                    <div className='bg-[#690808] p-1 rounded-xl flex justify-between items-center shadow-[0_5px_15px_rgba(0,0,0,0.4)]'>
+                        <div className='flex items-center gap-1'>
+                            <img src='/avatar2.png' className='w-4 h-4 rounded-full' />
+                            <span className='font-bold text-[10px]'>JOÃO LUCAS</span>
+                        </div>
+                        <button className='bg-[#3E0404] px-3 py-1 rounded-md text-[10px]'>Ver mais</button>
+                    </div>
+
+                    {/* ITEM 3 */}
+                    <div className='bg-[#690808] p-1 rounded-xl flex justify-between items-center shadow-[0_5px_15px_rgba(0,0,0,0.4)]'>
+                        <div className='flex items-center gap-1'>
+                            <img src='/avatar3.png' className='w-4 h-4 rounded-full' />
+                            <span className='font-bold text-[10px]'>NICHOLAS ALVES</span>
+                        </div>
+                        <button className='bg-[#3E0404] px-3 py-1 rounded-md text-[10px]'>Ver mais</button>
                     </div>
                 </Box>
             </Box>
 
             {/* GRÁFICO (Lado Direito) */}
-            <Box className='lg:w-1/2 flex flex-col items-center mt-6 lg:mt-0 bg-[#500000] border-3 border-[#3E0404] !rounded-[5px]'>
+            <Box className='lg:w-1/2 flex flex-col items-center mt-6 lg:mt-0 bg-[#500000] border-3 border-[#3E0404] !rounded-[5px] shadow-[0_5px_15px_rgba(0,0,0,0.4)]'>
                 <Typography variant='h5' className='!font-semibold text-white pt-4'>GRÁFICO</Typography>
                 <Box className='h-64 w-full mt-2 flex justify-around items-end p-2'>
                     {/* Barras de Exemplo */}
